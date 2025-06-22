a60c35a163ebc5fd725698e9c172d87655422f69
Oleh Kuzmenko <olehku@ext.inditex.com>
2025-06-22 22:17:12 +0300

    Added circuit breaker to feign clients

--- accounts/src/main/java/com/ol/app/accounts/infrastructure/client/cards/CardsClient.java
+++ accounts/src/main/java/com/ol/app/accounts/infrastructure/client/cards/CardsClient.java
@@ -3,13 +3,22 @@ package com.ol.app.accounts.infrastructure.client.cards;
 import com.ol.app.accounts.infrastructure.client.ClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "cards", path = "/api/v1/cards", configuration =  ClientConfig.class)
+@FeignClient(name = "cards", path = "/api/v1/cards", configuration =  ClientConfig.class, fallback = CardsClient.Fallback.class)
 public interface CardsClient {
 
   @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
   CardResponse getCard(@RequestParam String phone);
 
+  @Component
+  class Fallback implements CardsClient {
+
+    @Override
+    public CardResponse getCard(String phone) {
+      return new CardResponse(null, null, null, null, null, null, null, null, null, null, null);
+    }
+  }
 }
--- accounts/src/main/java/com/ol/app/accounts/infrastructure/client/loans/LoansClient.java
+++ accounts/src/main/java/com/ol/app/accounts/infrastructure/client/loans/LoansClient.java
@@ -1,15 +1,25 @@
 package com.ol.app.accounts.infrastructure.client.loans;
 
 import com.ol.app.accounts.infrastructure.client.ClientConfig;
+import com.ol.app.accounts.infrastructure.client.loans.LoansClient.Fallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "loans", path = "/api/v1/loans", configuration =  ClientConfig.class)
+@FeignClient(name = "loans", path = "/api/v1/loans", configuration =  ClientConfig.class, fallback = Fallback.class)
 public interface LoansClient {
 
   @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
   LoanResponse getLoan(@RequestParam String phone);
 
+  @Component
+  class Fallback implements LoansClient {
+
+    @Override
+    public LoanResponse getLoan(String phone) {
+      return new LoanResponse(null, null, null, null, null, null, null, null, null, null, null);
+    }
+  }
 }
